92e6c1a11f9ef04b09c38df4d2a4cbfc156d6ad0
Tombean <tom.somervilleroberts@gmail.com>
2016-03-23 10:50:10 +0100

    DVDPlayer added using Player interface

---
+++ CoursJavaIG3/src/audio/DVDPlayer.java
@@ -0,0 +1,29 @@
+package audio;
+
+public class DVDPlayer implements Player {
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reverse() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
