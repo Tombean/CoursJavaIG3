7d0e2733f63eccece496c2e978889ac0ed4c94ca
Tombean <tom.somervilleroberts@gmail.com>
2016-03-23 10:48:00 +0100

    Recorder and TapePlayer added

---
+++ CoursJavaIG3/src/audio/Recorder.java
@@ -0,0 +1,5 @@
+package audio;
+
+public interface Recorder {
+	void record();
+}
---
+++ CoursJavaIG3/src/audio/TapePlayer.java
@@ -0,0 +1,8 @@
+package audio;
+
+public class TapePlayer implements Recorder {
+	public void record(){
+		System.out.println("Silence, on enregistre ! ");
+	}
+	
+}
