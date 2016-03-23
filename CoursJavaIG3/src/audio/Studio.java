5456fb1314fd10a25832355fd754b56ee849ec9d
JohanBrunet <brunet.johan.23@gmail.com>
2016-03-23 10:57:02 +0100

    Modify Studio
    Add DVDPlayer to Studio

--- CoursJavaIG3/src/audio/Studio.java
+++ CoursJavaIG3/src/audio/Studio.java
@@ -5,12 +5,17 @@ public class Studio {
 	public static void main(String[] args) {
 		Player cdPlayer = new CDPlayer();
 		Player tapePlayer = new TapePlayer();
+		Player dvdPlayer = new DVDPlayer();
 		
 		cdPlayer.play();
 		((TapePlayer)tapePlayer).record();
+		dvdPlayer.play();
 		cdPlayer.pause();
 		cdPlayer.play();
+		dvdPlayer.pause();
 		cdPlayer.stop();
+		dvdPlayer.play();
+		dvdPlayer.stop();
 	}
 
 }
